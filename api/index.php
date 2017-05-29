4d19f1fd76b98421e74650d72b07b147a1893325
Victor <doc999tor@gmail.com>
2017-05-29 15:00:54 +0300

    Added default setnames utf8 to PDO settings

--- api/index.php
+++ api/index.php
@@ -17,7 +17,7 @@ $container = $app->getContainer();
 
 $container['db'] = function ($c) {
 	$db = $c['settings']['db'];
-	$pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'], $db['user'], $db['pass']);
+	$pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'] . ";charset=utf8", $db['user'], $db['pass']);
 	// $pdo = new PDO("sqlsrv:Server=" . $db['host'] . ";Database=" . $db['dbname']);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
